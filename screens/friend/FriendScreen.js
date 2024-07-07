6861c6955bb802759cfbbb711aaffbdee1eeebaa
MAC M1 <macm1@MACs-MacBook-Air.local>
2024-07-07 15:26:26 -0500

    newcode

--- screens/friend/FriendScreen.js
+++ screens/friend/FriendScreen.js
@@ -7,22 +7,28 @@ import { FlatList } from "react-native-gesture-handler";
 import PeopleItem from "../components/ItemPeople";
 import { useThemeContext } from "../../ThemeContext";
 import EditDelModal from "../../components/EditDelModal";
+
 const FriendScreen = ({ navigation }) => {
   const [friendList, setFriendList] = useState([]);
   const { setIsLoading } = useContext(AppContext);
+
   const getFriendList = async () => {
     // Call API to get friend list
     setIsLoading(true);
     try {
       const data = await AxiosInstance().get("friend/");
-      setFriendList(data);
+      // Filter friends to include only those with a profile picture
+      const filteredData = data.filter(friend => friend.profile_picture);
+      setFriendList(filteredData);
     } catch (err) {
       console.log(err);
     } finally {
       setIsLoading(false);
     }
   };
-  const {theme}=useThemeContext()
+
+  const { theme } = useThemeContext();
+
   useEffect(() => {
     getFriendList();
   }, []);
@@ -43,12 +49,15 @@ const FriendScreen = ({ navigation }) => {
       <FlatList
         data={friendList}
         keyExtractor={(item) => item.friend_id.toString()}
-        renderItem={({ item }) => <PeopleItem 
-          item={item} 
-          onPress={() => navigation.navigate("DetailScreen", { id: item.people_id })}
-        />}
+        renderItem={({ item }) => (
+          <PeopleItem 
+            item={item} 
+            onPress={() => navigation.navigate("DetailScreen", { id: item.people_id })}
+          />
+        )}
       />
     </View>
   );
 };
+
 export default FriendScreen;
